630a61a70e43f69d5594b122ea365d4d01c9aa29
Sybren W <sybren.willemot@gmail.com>
2024-11-13 22:59:44 +0100

    chore: reset

--- packages/playground/app.vue
+++ packages/playground/app.vue
@@ -1,13 +1,8 @@
 <script setup lang="ts">
-import { ForgeAvatar } from 'tiny-forge-ui'
 </script>
 
 <template>
   <div>
-    <ForgeAvatar
-      name="Sybren Willemot"
-      src="https://avatars.githubusercontent.com/u/16015740?v=4"
-      @status-change="(e) => console.log(e.status)"
-    />
+    <h1>Playground for Tiny Forge</h1>
   </div>
 </template>
